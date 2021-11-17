8060a084dd977f94ca1fcdc683ffd2b8e512204d
Lihsayuri <liviasm1@al.insper.edu.br>
2021-11-17 07:34:27 -0300

    Descobrindo o get dos standings...

--- src/components/Standings/standings.js
+++ src/components/Standings/standings.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
-// import { IconContext } from 'react-icons';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function Standings() {
-  
+
+  const [firstPlace, setFirstPlace] = useState("");
+  const [secondPlace, setSecondPlace] = useState("");
+  const [thirdPlace, setThirdPlace] = useState("");
+
+
+  const loadData = () => {
+    axios
+    .get("http://ergast.com/api/f1/current/driverStandings.json")
+    .then((res) => {
+        // setLastRaceResult(res.data);
+
+      console.log("ESSE É O RES.DATA: ", res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    });
+  }
+
+
+  useEffect(() => {
+      loadData();
+  }, []);
+
   return (
-    <p>HELLOOOOOOOOOOOOOOO F1</p>
+    <>
+      <p>OI</p>
+    </>
   );
 }
 
