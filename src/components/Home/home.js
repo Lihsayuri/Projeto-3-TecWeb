236d5ce421ce26590699e895c6a0bb7d6ea2de78
Lihsayuri <liviasm1@al.insper.edu.br>
2021-11-18 11:18:55 -0300

    Home mais arrumadin

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -5,30 +5,37 @@ import './home.css';
 
 function Home() {
 
-  const [firstPlace, setFirstPlace] = useState("");
-  const [secondPlace, setSecondPlace] = useState("");
-  const [thirdPlace, setThirdPlace] = useState("");
+  const [standings, setStandings] = useState("");
+  const [points, setPoints] = useState("");
 
-  const [qualiFirst, setQualiFirst] = useState("")
-  const [qualiSecond, setQualiSecond] = useState("")
-  const [qualiThird, setQualiThird] = useState("")
+  const [qualiFirst, setQualiFirst] = useState("");
+  const [qualiSecond, setQualiSecond] = useState("");
+  const [qualiThird, setQualiThird] = useState("");
 
 
-  const [season, setSeason] = useState("")
-  const [place, setPlace] = useState("")
+  const [season, setSeason] = useState("");
+  const [place, setPlace] = useState("");
+  const [city, setCity] = useState("");
 
   const loadData = () => {
     axios
     .get("http://ergast.com/api/f1/current/last/results.json")
     .then((res) => {
         // setLastRaceResult(res.data);
+        let colocacoes = []
+        let pontos = []
         setSeason(res.data.MRData.RaceTable.season);
         setPlace(res.data.MRData.RaceTable.Races[0].Circuit.Location.country+", "+ res.data.MRData.RaceTable.Races[0].Circuit.circuitName);
-        setFirstPlace(res.data.MRData.RaceTable.Races[0].Results[0].Driver.givenName+" "+res.data.MRData.RaceTable.Races[0].Results[0].Driver.familyName);
-        setSecondPlace(res.data.MRData.RaceTable.Races[0].Results[1].Driver.givenName+" "+res.data.MRData.RaceTable.Races[0].Results[1].Driver.familyName);
-        setThirdPlace(res.data.MRData.RaceTable.Races[0].Results[2].Driver.givenName+" "+res.data.MRData.RaceTable.Races[0].Results[2].Driver.familyName);
+        setCity(res.data.MRData.RaceTable.Races[0].raceName)
+        for (let i = 0; i < res.data.MRData.RaceTable.Races[0].Results.length ; i++ ){
+          colocacoes.push(res.data.MRData.RaceTable.Races[0].Results[i].Driver.givenName+" "+res.data.MRData.RaceTable.Races[0].Results[i].Driver.familyName);
+          pontos.push(res.data.MRData.RaceTable.Races[0].Results[i].points);
+        }
 
-        // console.log("ESSE É O RES.DATA: ", res.data.MRData.RaceTable.Races[0]);
+        setStandings(colocacoes);
+        setPoints(pontos);
+        console.log("ESSE É O RES.DATA: ", res.data.MRData.RaceTable.Races[0]);
+        console.log("OLHA AI: ", colocacoes)
     });
     axios
     .get("http://ergast.com/api/f1/current/last/qualifying.json")
@@ -52,9 +59,22 @@ function Home() {
     <main>
       <h3 className="lastRace"> {place}</h3>
       <h4 className="year"> {season} </h4>
-      Primeiro colocado: {firstPlace} <br/>
-      Segundo colocado: {secondPlace} <br/>
-      Terceiro colocado: {thirdPlace} <br/>
+      <h5 className="city"> {city} </h5>  
+      
+      <div className="row">
+        <div className="columns">
+            {standings.map((position) => (
+              <p>{position}</p>
+                ))}
+        </div>
+
+        <div className="columns">
+            {points.map((point) => (
+              <p>{point}</p>
+                ))}
+        </div>
+      </div>
+
 
       Quali: <br/>
       Primeiro mais rápido quali: {qualiFirst} <br/>
