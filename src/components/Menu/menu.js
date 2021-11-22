1d2ca0a11e768c22fd72a99df68a767079b47e6b
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-11-22 20:49:32 -0300

    links para os times com estilo funcionando

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './menu.css';
 // import { IconContext } from 'react-icons';
 
 function Menu() {
+  const [teamDiv, setTeamDiv] = useState();
+  useEffect(()=>{
+    setTeamDiv(document.getElementById("teams-links"))
+    
+  }, [])
   
+  function showTeams(){
+    teamDiv.style.display="flex";
+  }
+  function hideTeams(){
+    teamDiv.style.display="none";
+  }
   return (
     <div className="menu-holder">
       <div className="header">
@@ -19,13 +30,13 @@ function Menu() {
           <Link className="link" to="/constructor-standings">
               Team standings
           </Link>
-          <button id="a" className="teams-button">
+          <button id="a" className="teams-button" onMouseOver={showTeams}>
             Teams
           </button>
         </div>
       </div>
         
-      <div id="b" className="teams-div">
+      <div id="teams-links" className="teams-div" onMouseLeave={hideTeams}>
         <Link className="teams-link" to=''>
           Alfa Romeo
         </Link>
