961f999a11af71822c97ebb5dd880b4c156841eb
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-11-22 19:25:05 -0300

    Trabalhando no menu

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -6,20 +6,57 @@ import './menu.css';
 function Menu() {
   
   return (
-    <div className="header">
-      <img src="/f1_logo.png" className="f1Logo" />
-      <div className="links-box">
-        <Link className="link" to="/">
-            Home
-        </Link>
-        <Link className="link" to="/driver-standings">
-            Driver standings
-        </Link>
-        <Link className="link" to="/constructor-standings">
-            Team standings
-        </Link> 
+    <div className="menu-holder">
+      <div className="header">
+        <img src="/f1_logo.png" className="f1Logo" />
+        <div className="links-box">
+          <Link className="link" to="/">
+              Home
+          </Link>
+          <Link className="link" to="/driver-standings">
+              Driver standings
+          </Link>
+          <Link className="link" to="/constructor-standings">
+              Team standings
+          </Link>
+          <button id="a" className="teams-button">
+            Teams
+          </button>
+        </div>
       </div>
-      
+        
+      <div id="b" className="teams-div">
+        <Link className="teams-link" to=''>
+          Alfa Romeo
+        </Link>
+        <Link className="teams-link" to=''>
+          alphaTauri
+        </Link>
+        <Link className="teams-link" to=''>
+          Alpine
+        </Link>
+        <Link className="teams-link" to=''>
+          Aston Martin
+        </Link>
+        <Link className="teams-link" to=''>
+          Ferrari
+        </Link>
+        <Link className="teams-link" to=''>
+          Haas
+        </Link>
+        <Link className="teams-link" to=''>
+          McLaren
+        </Link>
+        <Link className="teams-link" to=''>
+          Mercedes
+        </Link>
+        <Link className="teams-link" to=''>
+          Red Bull
+        </Link>
+        <Link className="teams-link" to=''>
+          Williams
+        </Link>
+      </div>
     </div>
   );
 }
