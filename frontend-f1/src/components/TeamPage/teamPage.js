520f1b02c1b3825a3f050f8c887fb3e8fd6d9be9
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-11-27 18:15:13 -0300

    limpando importação

--- frontend-f1/src/components/TeamPage/teamPage.js
+++ frontend-f1/src/components/TeamPage/teamPage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import './teamPage.css'
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function TeamPage(){
     //window.location.reload();
